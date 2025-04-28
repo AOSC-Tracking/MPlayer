cbf06ddbcfd2d43337e63c4432c7d9947560d8a0
CAB233 <yidaduizuoye@outlook.com>
2025-04-28 18:50:18 +0800

    [r38392] mplayer.c: Fix crash introduced in r38380.
    When we fail to read video properties we need
    to ensure sh_video is set to NULL so we do not
    try to play video.
    
    (From upstream SVN r38392)

--- mplayer.c
+++ mplayer.c
@@ -2435,6 +2435,9 @@ int reinit_video_chain(void)
 
 err_out:
     uninit_player(INITIALIZED_VCODEC);
+    // ensure we do not try to play video even if we
+    // failed before vfilter creation.
+    mpctx->sh_video = NULL;
     return 0;
 }
 
